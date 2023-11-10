b4016dc1e64fc8933fbb0ab13763e9f20452e7b5
wiwsic <82441666+wiwsic@users.noreply.github.com>
2023-11-10 01:38:18 -0300

    06_leapYears done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,7 @@
-const leapYears = function() {
+const leapYears = function(num) {
+
+    if ((num % 100 === 0 &&  num % 400 === 0) || (num % 4 === 0 && (!(num % 100 === 0)))) return true
+    else return false;
 
 };
 
